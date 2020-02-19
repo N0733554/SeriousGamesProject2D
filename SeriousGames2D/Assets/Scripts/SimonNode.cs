bcca1ad99ac75ee200275e33a9fad853c9e68d42
N0733554 <43959754+N0733554@users.noreply.github.com>
2020-02-19 18:22:41 +0000

    Added first iteration of Simon Says Module

---
+++ SeriousGames2D/Assets/Scripts/SimonNode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimonNode : Node
+{
+    SimonModule module;
+    
+    protected override void Start()
+    {
+        base.Start();
+        module = GetComponentInParent<SimonModule>();
+    }
+
+    protected override void Update()
+    {
+        base.Update();             
+    }    
+}
